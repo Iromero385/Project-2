be492b4a60de3eaf50b5108e11d63e7d898c0b64
Natalie Montano <nat.montano366@gmail.com>
2019-10-23 00:37:13 -0700

    setting up connections

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Jumbotron} from 'react-bootstrap';
+import {Home} from './components/Home/'
 
+import './App.css';
+//creates a wrapper inst. div components
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/contact" component={Comments}/>
+          <Route exact path="/signup" component={SignUp}/>
+          <Route exact path="/login" component={Login}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </Router>
+
+
+    </React.Fragment>
   );
 }
 
